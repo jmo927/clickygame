203ae089a867a895162e13baeec1b8b0685457f9
jmo <40837639+jmo927@users.noreply.github.com>
2019-01-29 19:16:27 -0600

    Updates

---
+++ precache-manifest.59a52fe880ff077f0b124ec0944d8a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickygame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "c5a41c72f7fa1edbc81b",
+    "url": "/clickygame/static/js/main.c5a41c72.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickygame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c5a41c72f7fa1edbc81b",
+    "url": "/clickygame/static/css/main.9e5c8faf.chunk.css"
+  },
+  {
+    "revision": "415be1ece26577887a8e31496cad5d03",
+    "url": "/clickygame/index.html"
+  }
+];
